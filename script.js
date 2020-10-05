81b75dc0dffe1d02f3432ec769e97efbf17bb076
dvramos <dvramos@hotmail.com.br>
2020-10-05 07:16:47 -0300

    4/6 DIO
    Comparação entre FirstCard e SecondCard, checkFroMath -> JS
    
    DataCard -> HTML

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+    checkFroMath();
+}
+
+function checkForMath(){
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCard();
+        return;
+    }
+
+    unflipCards();
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
+
