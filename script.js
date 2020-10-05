3e7255b20c8414f62c9cb5313a3378aacdbfa8f7
dvramos <dvramos@hotmail.com.br>
2020-10-05 07:45:45 -0300

    6/7 DIO
    lockBoard, resetBoard.

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
+    if(this === firstCard) return;
 
-    
+
     this.classList.add('flip');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -30,16 +31,26 @@ function checkForMath(){
 function disableCards(){
  firstCard.removeEventListener('click', flipCard);
  secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards(){
+    lockBoard = true;
     setTimeout (()=> {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
-})
+});
 
